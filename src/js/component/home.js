86f7baab1a77a2f250cc9738058035d695c744db
TAPIAR7 <rtapiatec@gmail.com>
2021-03-08 23:26:16 +0000

    Simple counter completed

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+function SecondsCounter(props) {
+	return <div></div>;
+}
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
+		<div className="text-center mt-5 bg-dark">
+			<h1 className="text-white">
+				<i className="far fa-clock"></i> {props.seconds}
+			</h1>
+			{/* <p>
 				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
+			</p> */}
+			{/* <a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working
 			</a>
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
-			</p>
+			</p> */}
 		</div>
 	);
 }
+
+Home.propTypes = {
+	seconds: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,29 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 
+function stringZeros(value) {
+	let amountZeros = 5;
+	let zerosTrail = "0";
+	if (value.length <= amountZeros) {
+		for (let index = 1; index < amountZeros - value.length; index++) {
+			zerosTrail = zerosTrail + "0";
+		}
+	} else {
+		zerosTrail = "";
+	}
+	console.log(amountZeros);
+	return zerosTrail;
+}
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let secondsNew = 0;
+function updateCounter() {
+	ReactDOM.render(
+		<Home
+			seconds={stringZeros(secondsNew.toString()) + secondsNew.toString()}
+		/>,
+		document.querySelector("#app")
+	);
+	secondsNew += 1;
+	console.log(secondsNew);
+}
+setInterval(updateCounter, 1000);
